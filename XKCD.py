640b4a564037bc56270a282cf8eb67528dbaa4b0
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-03-11 14:27:34 +0000

    Completed XKCD image scraper, using both beautiful soup 4 & Regex

---
+++ XKCD.py
@@ -0,0 +1,40 @@
+import re
+import requests
+import bs4
+import urllib.request
+import time
+
+
+def xkcd_beautiful_soup():
+    res = requests.get('http://xkcd.com')
+    res.raise_for_status()
+    regex = re.compile(r'https://xkcd.com/(\d+)/')
+    page_total = re.findall(regex, res.text)
+    skip_list = [404,1350,1416,1446,1608,1663]
+    for page in range(1,int(page_total[0])+1):
+        print(page)
+        if page in skip_list:
+            print(page," Skipped")
+            continue
+        res = requests.get('http://xkcd.com/{}/'.format(page))
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+        image = (soup.find('div', attrs={'id': 'comic'}).img.get('src'))
+        urllib.request.urlretrieve("http://xkcd.com/{}".format(image), "XKCD/beautiful_soup/{}.jpg".format(page))
+        print("Complete: {}".format(page))
+
+def xkcd_regex():
+    res = requests.get('http://xkcd.com')
+    res.raise_for_status()
+    regex = re.compile(r'https://xkcd.com/(\d+)/')
+    page_total = re.findall(regex, res.text)
+    regex = re.compile(r'(https://imgs.xkcd.com/comics/.*)')
+    for page in range(int(page_total[0]),0,-1):
+        print("Starting: {}".format(page))
+        res = requests.get('http://xkcd.com/{}/'.format(page))
+        res.raise_for_status()
+        re_image_link = re.findall(regex,res.text)
+        urllib.request.urlretrieve(re_image_link[0], "XKCD/RE/{}.jpg".format(page))
+        print("Complete: {}".format(page))
+
+xkcd_regex()
